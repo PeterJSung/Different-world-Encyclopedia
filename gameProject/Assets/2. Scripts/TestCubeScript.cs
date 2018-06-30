4694de2c675255c15892a30a9eabdb5bd58be36d
jungmin.sung <ivsinji746@naver.com>
2018-06-30 22:30:01 +0900

    Initial commit
    InitialCommit

---
+++ gameProject/Assets/2. Scripts/TestCubeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCubeScript : MonoBehaviour {
+
+    Vector3Int rotateVec;
+	// Use this for initialization
+	void Start () {
+        rotateVec.x = rotateVec.y = rotateVec.z = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotateVec.x++;
+        rotateVec.y++;
+        rotateVec.z++;
+        transform.rotation = Quaternion.Euler(rotateVec.x, rotateVec.y, rotateVec.z);
+    }
+}
