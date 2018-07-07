9c13ae0833ca0d754ad79fbc66a6b511285975f3
jungmin.sung <ivsinji746@naver.com>
2018-07-07 15:00:24 +0900

    기본방식 변경

--- Different world Encyclopedia/Assets/Script/Playermove.cs
+++ Different world Encyclopedia/Assets/Script/Playermove.cs
@@ -7,13 +7,8 @@ public class Playermove : MonoBehaviour
     public const float MOVE_WEIGHT = 0.04f;
     public const float DASH_MOVE_WEIGHT = 0.08f;
     Rigidbody2D rigid2D;
-    float jumpForce = 400.0f;
-    float walkForce = 30.0f;
-    float maxWalkSpeed = 3.1f;
-    float dashForce = 800.0f;
-    float time = 0f;
-    float dashForce2 = 800.0f;
-    float time2 = 0f;
+    public const float JUMP_FORCE = 400.0f;
+   
     public int jumpcount = 2;
     public bool isGrounded = false;
 
@@ -42,10 +37,23 @@ public class Playermove : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D col)
 
     {
+        switch (col.gameObject.tag)
+        {
+            case "Tile":
 
-        if (col.gameObject.tag == "Ground")
-            isGrounded = true;
-        jumpcount = 2;
+                TileType nowTile = col.gameObject.GetComponent<TileScript>().SelectTileType;
+                if (nowTile == TileType.GROUND ||
+                    nowTile == TileType.FLOAT_GROUND)
+                {
+                    isGrounded = true;
+                    jumpcount = 2;
+                }
+                break;
+            case "Monster":
+                break;
+            case "Trap":
+                break;
+        }
     }
 
 
@@ -57,11 +65,36 @@ public class Playermove : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.X) && jumpcount > 0)
 
             {
-                this.rigid2D.AddForce(transform.up * this.jumpForce);
+                this.rigid2D.AddForce(transform.up * JUMP_FORCE);
                 jumpcount--;
             }
         }
 
+        CheckMove();
+
+
+        this.transform.Translate(moveValue.moveWeight, 0, 0);
+
+        
+
+        // 캐릭터 스프라이트 반전
+        if (moveValue.moveWeight > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveValue.moveWeight < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    void CheckJump()
+    {
+
+    }
+
+    void CheckMove()
+    {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             if (moveValue.prevValue == KeyCode.RightArrow && Time.time - moveValue.tDown < 0.5f)
@@ -93,20 +126,6 @@ public class Playermove : MonoBehaviour
         {
             moveValue.moveWeight = 0;
         }
-        this.transform.Translate(moveValue.moveWeight, 0, 0);
-
-        
-
-        // 캐릭터 스프라이트 반전
-        if (moveValue.moveWeight > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (moveValue.moveWeight < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-
     }
 
     void DEBUG_KEY_FUNCTION()
---
+++ Different world Encyclopedia/Assets/Script/TileScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour, ITile {
+    public TileType SelectTileType;
+    public float SelectHarzardValue;
+
+    TileType ITile.tileType
+    {
+        get {
+            return SelectTileType;
+        }
+        set {
+            SelectTileType = value;
+        }
+    }
+    float ITile.harzardValue {
+        get
+        {
+            return SelectHarzardValue;
+        }
+        set
+        {
+            SelectHarzardValue = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
